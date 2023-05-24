80027e35bb9ee1ad139fa2ebe1d9de80d491610c
Jasser Tayari <jassertayari101@gmail.com>
2023-05-24 15:06:04 +0200

    implemented dice roll animation

--- script.js
+++ script.js
@@ -8,14 +8,60 @@ function namePlayer(playerNumber){
 }
 
 
+//key is number of rolled dice and the value is an array of points to hide
+const diceGuide = {
+    1 : ['one'],
+    2 : ['three','five'],
+    3 : ['three','one','five'],
+    4 : ['two','three','four','five'],
+    5 : ['two','three','four','five','one'],
+    6 : ['two','three','four','five','six','seven'],
+    7 : ['two','three','four','five','six','seven','one'],
+}
+
+
+function ShowPoints(points){
+    points.forEach(point=>{
+        const pointToShow = document.querySelector(`#dice .${point}`);
+        pointToShow.classList.toggle('hidden');
+    })
+}
+function HideAll(){
+    const points = Array.from(document.querySelectorAll('#dice .point'));
+    points.forEach(point=>{
+        point.classList.add('hidden');
+    })
+}
+
+function animateDice(rolled){
+    HideAll();
+    ShowPoints(diceGuide[rolled]);
+}
+
+
+
+
 const currents = Array.from( document.querySelectorAll(".current em") );
 console.log(currents);
 
 let currentPlayer = 0;
-
+let currentScore = 0;
 const btnRoll = document.querySelector(".roll");
 
 btnRoll.addEventListener('click',()=>{
-    console.log(`Player ${namePlayer(currentPlayer)} rolled ${roll()}.`);
-    currentPlayer++;
+    let rolled = roll();
+    animateDice(rolled);
+    let playerName = namePlayer(currentPlayer);
+    
+    if(rolled == 1){
+        console.log(`Player ${playerName} rolled a one : RESET!`);
+        currentScore = 0;
+        currentPlayer++;
+    }else{
+        console.log(`Player ${playerName} rolled ${rolled}.`);
+        currentScore+=rolled;
+        console.log(`Player ${playerName}'s score : ${currentScore}`);
+    }
+    
+    
 });
